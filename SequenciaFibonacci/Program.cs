46174ffc6b49d2fc015e16521fd8e955619e98f6
Matheus Gress <matheusaugustogress@gmail.com>
2024-09-07 01:31:53 -0300

    feat: add project to check for specific character in a string

---
+++ SequenciaFibonacci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
+class Program
+{
+    static bool IsPerfectSquare(int x)
+    {
+        int square = (int)Math.Sqrt(x);
+        return square * square == x;
+    }
+
+    static void Main()
+    {
+        // Verifica se o valor recebido mão é null
+        try
+        {
+            Console.WriteLine("Digite o valor que deseja verificar na lista:");
+            string? input = Console.ReadLine() ?? throw new ArgumentNullException();
+
+            int number = int.Parse(input);
+            
+        // Verifica se o valor é uma raiz exata
+            int sumCheck = 5 * number * number + 4;
+            int subCheck = 5 * number * number - 4;
+
+            if (IsPerfectSquare(sumCheck) || IsPerfectSquare(subCheck))
+            {
+                Console.WriteLine($"\nO número {number} pertence à sequência Fibonacci");
+            }
+            else
+            {
+                Console.WriteLine($"\nO número {number} não pertence à sequência Fibonacci");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+    }
+}
---
+++ VerificaLetra/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Formats.Asn1;
+
+class Program
+{
+    static void Main()
+    {
+        string? input;
+
+        // Verifica se a entrada não é vazia
+        do
+        {
+            Console.Write("Digite a palavra que deseja verificar: ");
+            input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("A entrada não pode ser vazia. Por favor, tente novamente.");
+            }
+
+        } while (string.IsNullOrWhiteSpace(input));
+
+        int lowercaseACount = 0;
+        int uppercaseACount = 0;
+
+        // Loop para verificar cada caractere
+        foreach (char c in input)
+        {
+            if (c == 'a')
+            {
+                lowercaseACount++;
+            }
+            else if (c == 'A')
+            {
+                uppercaseACount++;
+            }
+        }
+
+        int totalACount = lowercaseACount + uppercaseACount;
+        
+        if (totalACount > 0)
+        {
+            Console.WriteLine($"\nA letra 'A' foi encontrada {totalACount} vez(es).");
+        }
+        else
+        {
+            Console.WriteLine("\nA letra 'A' não foi encontrada na palavra.");
+        }
+    }
+}
