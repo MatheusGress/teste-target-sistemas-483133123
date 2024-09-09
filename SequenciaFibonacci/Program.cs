bdeb540616a45421bacabc12a59ab74ed4e4a36b
MatheusGress <matheusaugustogress@gmail.com>
2024-09-09 15:30:41 -0300

    refactor: change input validation method from try-catch to if-else for cleaner structure

--- SequenciaFibonacci/Program.cs
+++ SequenciaFibonacci/Program.cs
@@ -10,38 +10,35 @@
 
         static void Main()
         {
-            // Verifica se o valor recebido não é null
-            try
-            {
-                Console.WriteLine("Digite o valor que deseja verificar na lista:");
-                string? input = Console.ReadLine() ?? throw new ArgumentNullException("O valor de entrada não pode ser nulo.");
+            Console.WriteLine("Digite o valor que deseja verificar na lista:");
+            string? input = Console.ReadLine();
 
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Erro: A entrada não pode estar vazia ou ser apenas espaços em branco.");
+            }
+            else
+            {
+                // Tenta converter a entrada para um número inteiro
                 if (!int.TryParse(input, out int number))
                 {
-                    Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro.");
-                    return;
-                }
-
-                // Verifica se o valor é uma raiz exata
-                int sumCheck = 5 * number * number + 4;
-                int subCheck = 5 * number * number - 4;
-
-                if (IsPerfectSquare(sumCheck) || IsPerfectSquare(subCheck))
-                {
-                    Console.WriteLine($"\nO número {number} pertence à sequência Fibonacci");
+                    Console.WriteLine("Erro: A entrada não é um número inteiro válido.");
                 }
                 else
                 {
-                    Console.WriteLine($"\nO número {number} não pertence à sequência Fibonacci");
+                    // Verifica se o valor é uma raiz exata
+                    int sumCheck = 5 * number * number + 4;
+                    int subCheck = 5 * number * number - 4;
+
+                    if (IsPerfectSquare(sumCheck) || IsPerfectSquare(subCheck))
+                    {
+                        Console.WriteLine($"\nO número {number} pertence à sequência Fibonacci");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nO número {number} não pertence à sequência Fibonacci");
+                    }
                 }
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Erro: O valor inserido não é um número válido.");
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine($"Erro: {e.Message}");
             }
         }
     }
